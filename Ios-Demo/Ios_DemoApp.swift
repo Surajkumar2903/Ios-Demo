3370ba151b70910c79f88659efda5f6c15584b69
Surajkumar2903 <sk5693747@gmail.com>
2025-12-11 12:52:15 +0530

    Initial commit

---
+++ Ios-Demo/Ios_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ios_DemoApp.swift
+//  Ios-Demo
+//
+//  Created by Suraj Kumar on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Ios_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
