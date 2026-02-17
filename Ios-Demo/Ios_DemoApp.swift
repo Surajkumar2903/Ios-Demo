4ddf0e7118d709efefe6e0f9dc30df588142a9fa
Surajkumar2903 <sk5693747@gmail.com>
2026-02-17 12:36:15 +0530

    url Updated for .co

--- Ios-Demo/Ios_DemoApp.swift
+++ Ios-Demo/Ios_DemoApp.swift
@@ -27,14 +27,29 @@ struct Ios_DemoApp: App {
             NavigationView {
                             if authManager.isUserLoggedIn {
                                 ContentView()
+                                
                                     .withAppStorysOverlays()
-                                    .task {
-                                        AppStorys.initialize(
-                                            accountID: "4350bf8e-0c9a-46bd-b953-abb65ab21d11",
-                                            appID: "9e1b21a2-350a-4592-918c-2a19a73f249a",
-                                            userID: "Suaj_SDK_V1"
-                                        )
-                                    }
+                                                   .task {
+                                                       // Optional: enable SDK logging for development
+                                                       AppStorysLogger.logLevel = .debug
+
+                                                       AppStorys.initialize(
+                                                           accountID: "12a9eac5-94ee-4735-9aa6-b8a94cb8fbbb",
+                                                           appID: "f69bdccf-b20f-4938-b39e-7075d76db791",
+                                                           userID: "Suaj_SDK_V1",
+                                                           baseURL: "https://users.appstorys.co"
+                                                       )
+                                                   }
+                                
+                                //MARK: Previous initialisation
+//                                    .withAppStorysOverlays()
+//                                    .task {
+//                                        AppStorys.initialize(
+//                                            accountID: "412a9eac5-94ee-4735-9aa6-b8a94cb8fbbb",
+//                                            appID: "f69bdccf-b20f-4938-b39e-7075d76db791",
+//                                            userID: "Suaj_SDK_V1"
+//                                        )
+//                                    }
                                 
                             } else {
                                 SignInView()
